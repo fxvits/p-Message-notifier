d1543e3e149ae98a9daa2fa46929324ec95bb0a2
fxvits <fxvits@hotmail.com>
2020-07-21 19:05:18 +0200

    8 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: 'app-root',
+  templateUrl: './app.component.html'
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
