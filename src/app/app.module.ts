9a4ebcf027c7b5da282e8e0a20ae7d6b1941bc11
fxvits <fxvits@hotmail.com>
2020-07-21 18:38:27 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import  { MessageModule, MessagesModule } from 'primeng'
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { AppService } from './app.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ],
-  providers: [AppService]
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    MessageModule,
+    MessagesModule
+  ],
+  declarations: [
+    AppComponent
+    ],
+  bootstrap:    [
+    AppComponent
+    ],
+  providers: [
+    AppService
+    ]
 })
 export class AppModule { }
