721f38748934b544002b78fac26bb3cc78de10bf
fxvits <fxvits@hotmail.com>
2020-07-21 18:49:34 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import {ToastModule} from 'primeng/toast';
 
@@ -9,6 +10,7 @@ import { AppService } from './app.service';
 @NgModule({
   imports:      [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     ToastModule,
 
